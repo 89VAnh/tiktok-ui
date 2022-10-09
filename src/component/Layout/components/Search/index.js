3adc48e7ab27bd8d59486bd0210219f26a5aa976
89VAnh <vietanha9a2@gmail.com>
2022-10-09 11:00:05 +0700

    Axios

--- src/component/Layout/components/Search/index.js
+++ src/component/Layout/components/Search/index.js
@@ -1,5 +1,7 @@
 import { useEffect, useState, useRef } from 'react';
+import { useDebounce } from '~/hooks';
 
+import * as searchServices from '~/apiServices/searchServices';
 import HeadlessTippy from '@tippyjs/react/headless';
 import { Wrapper as PopperWrapper } from '~/component/Popper';
 import AccountItem from '~/component/AccountItem';
@@ -18,25 +20,30 @@ function Search() {
     const [showResults, setShowResults] = useState(true);
     const [loading, setLoading] = useState(false);
 
+    const debounced = useDebounce(searchValue, 500);
+
     const inputRef = useRef();
 
     useEffect(() => {
-        if (!searchValue.trim()) {
+        if (!debounced.trim()) {
             setSearchResult([]);
             return;
         }
+
         setLoading(true);
 
-        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`)
-            .then((res) => res.json())
-            .then((res) => {
-                setSearchResult(res.data);
-                setLoading(false);
-            })
-            .catch(() => {
-                setLoading(false);
-            });
-    }, [searchValue]);
+        const fetchApi = async () => {
+            setLoading(true);
+
+            const result = await searchServices.search(debounced);
+
+            setSearchResult(result);
+
+            setLoading(false);
+        };
+
+        fetchApi();
+    }, [debounced]);
 
     const handlClear = () => {
         setSearchValue('');
